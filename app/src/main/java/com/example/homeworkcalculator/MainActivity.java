f0cc6aaf0cd716a931dba9719896fb3c8f58bed0
Michael Lee <michael830713@gmail.com>
2019-01-25 11:31:17 +0800

    fisish all the id naming

--- app/src/main/java/com/example/homeworkcalculator/MainActivity.java
+++ app/src/main/java/com/example/homeworkcalculator/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.homeworkcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView one;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void setupUIViews(){
+        one = (ImageView) findViewById(R.id.buttonOne);
     }
 }
