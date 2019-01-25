e960f4c9f1cd4306cada2ea40e4110b89b0907c6
Michael Lee <michael830713@gmail.com>
2019-01-25 18:22:29 +0800

    added function to button one

--- app/src/main/java/com/example/homeworkcalculator/MainActivity.java
+++ app/src/main/java/com/example/homeworkcalculator/MainActivity.java
@@ -2,19 +2,35 @@ package com.example.homeworkcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView one;
+    private TextView formula;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupUIViews();
+        one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                formula.setText(formula.getText().toString() + "1");
+            }
+        });
+
+
+
+
+
     }
 
     private void setupUIViews(){
         one = (ImageView) findViewById(R.id.buttonOne);
+        formula = (TextView) findViewById(R.id.CalculationFormula);
     }
 }
