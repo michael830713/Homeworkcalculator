6e7c26ba3826d8b2ddcd367c9214416079ab6d4c
Michael Lee <michael830713@gmail.com>
2019-01-25 18:58:16 +0800

    added hint for formula

--- app/src/main/java/com/example/homeworkcalculator/MainActivity.java
+++ app/src/main/java/com/example/homeworkcalculator/MainActivity.java
@@ -8,21 +8,90 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView zero;
     private ImageView one;
+    private ImageView two;
+    private ImageView three;
+    private ImageView four;
+    private ImageView five;
+    private ImageView six;
+    private ImageView seven;
+    private ImageView eight;
+    private ImageView nine;
+    private ImageView add;
+    private ImageView minus;
+    private ImageView multiply;
+    private ImageView divide;
     private TextView formula;
+    private TextView result;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setupUIViews();
+
+        zero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                formula.setText(formula.getText().toString() + "0");
+            }
+        });
         one.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 formula.setText(formula.getText().toString() + "1");
             }
         });
-
+        two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                formula.setText(formula.getText().toString() + "2");
+            }
+        });
+        three.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                formula.setText(formula.getText().toString() + "3");
+            }
+        });
+        four.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                formula.setText(formula.getText().toString() + "4");
+            }
+        });
+        five.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                formula.setText(formula.getText().toString() + "5");
+            }
+        });
+        six.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                formula.setText(formula.getText().toString() + "6");
+            }
+        });
+        seven.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                formula.setText(formula.getText().toString() + "7");
+            }
+        });
+        eight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                formula.setText(formula.getText().toString() + "8");
+            }
+        });
+        nine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                formula.setText(formula.getText().toString() + "9");
+            }
+        });
 
 
 
@@ -30,7 +99,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setupUIViews(){
+        zero = (ImageView) findViewById(R.id.buttonZero);
         one = (ImageView) findViewById(R.id.buttonOne);
+        two = (ImageView) findViewById(R.id.buttonTwo);
+        three = (ImageView) findViewById(R.id.buttonThree);
+        four = (ImageView) findViewById(R.id.buttonFour);
+        five = (ImageView) findViewById(R.id.buttonFive);
+        six = (ImageView) findViewById(R.id.buttonSix);
+        seven = (ImageView) findViewById(R.id.buttonSeven);
+        eight = (ImageView) findViewById(R.id.buttonEight);
+        nine = (ImageView) findViewById(R.id.buttonNine);
+        add = (ImageView) findViewById(R.id.buttonPlus);
+        minus = (ImageView) findViewById(R.id.buttonMinus);
+        multiply = (ImageView) findViewById(R.id.buttonMultiply);
+        divide = (ImageView) findViewById(R.id.buttonDivide);
+
+
         formula = (TextView) findViewById(R.id.CalculationFormula);
     }
 }
